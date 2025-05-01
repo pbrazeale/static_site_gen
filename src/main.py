1d9506e3ba1b5bf806889abeabf7d888f3d3612c
pbrazeale <pbrazeale@gmail.com>
2025-05-01 10:01:24 -0500

    0.7.2 Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from textnode import *
 
 def main():
     new_node = TextNode("anchor text", "link", "https://www.boot.dev")
-    print(__repr__(new_node))
+    print(repr(new_node))
 
 main()
--- src/textnode.py
+++ src/textnode.py
@@ -9,16 +9,16 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode():
-    def __init__(text, type, url=None):
+    def __init__(self, text, type, url=None):
         self.text = text
         self.text_type = type
         self.url = url
     
-def __eq__(node1, node2):
-    if (node1.text == node2.text and node1.text_type == node2.text_type and node1.url == node2.url):
-        return True
-    return False
+    def __eq__(self, node2):
+        if (node1.text == node2.text and node1.text_type == node2.text_type and node1.url == node2.url):
+            return True
+        return False
 
-def __repr__(node):
-    return f"TextNode({node.text}, {node.text_type}, {node.url})"
+    def __repr__(node):
+        return f"TextNode({node.text!r}, {node.text_type}, {node.url!r})"
 
