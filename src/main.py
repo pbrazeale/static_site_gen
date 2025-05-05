e0fe5fa998328b583ee4b7c9210b0049729c9970
pbrazeale <pbrazeale@gmail.com>
2025-05-05 14:22:41 -0500

    0.21.2 delete public/

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 import os
+import shutil
 
 def main():
     build_site()
 
 def build_site():
     # first delete public/...
+    if os.path.exists("../public/"):
+        shutil.rmtree("../public/")
+
+
 
     # copy all dir/files and subdir/files from "static/" to "public/"
 
